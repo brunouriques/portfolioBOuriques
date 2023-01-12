81fa9cfef722657c4905441f0d3ac6524cb51fa1
brunouriques <92872292+brunouriques@users.noreply.github.com>
2023-01-12 01:03:23 -0300

    Fix apply to navbar and title image not beeing responsive.

---
+++ script.js
@@ -0,0 +1,50 @@
+<!-- Navbar Start -->
+
+<div class="container-fluid" id="navigation">
+
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <a class="navbar-brand" href="">Bruno Ouriques</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navMenu">
+      <ul class="navbar-nav ml-auto">
+        <li class="navItem">
+          <a class="navLink" href="#about">Sobre</a>
+        </li>
+        <li class="navItem">
+          <a class="navLink" href="#projects">Projetos</a>
+        </li>
+        <li class="navItem">
+          <a class="navLink" href="#contact">Contato</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="navRight">
+      <a class="navSun" href="#">
+        <i class="fa-solid fa-sun"></i>
+      </a>
+    </div>
+  </nav>
+
+  </div>
+
+
+<!-- Navbar End -->
+
+<div class="lgBlueSection">
+
+  <!-- Title Page Start -->
+
+<div id="title">
+  <div class="container-fluid">
+    <img class="titleImg" src="CSS/title.jpg" alt="Image by lucabravo on Freepik">
+    <div class="titleGlass"><div class="titleText">Bruno Ouriques</div></div>
+  </div>
+</div>
+
+  <!-- Title Page End -->
+
+</div>
